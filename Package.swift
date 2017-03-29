59ab0da2bc59018307b1c596650029cb1fe4a280
maksym.koshtenko <maxim.koshtenko@auto1.com>
2017-03-29 15:17:56 +0300

    Update Vapor's version to 1.5

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "Stosyk",
     dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 1)
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 5)
     ],
     exclude: [
         "Config",
