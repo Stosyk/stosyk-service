ba1cb7d1ed93d5be95a57e91f782518060a96844
maksym.koshtenko <maxim.koshtenko@auto1.com>
2017-05-10 00:20:49 +0300

    Add possibily to separate test dependencies in package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,8 @@
+import Foundation
 import PackageDescription
+
+
+let addTestDependencies = ProcessInfo.processInfo.environment["SWIFTPM_TEST_TARGET"] == "YES"
 
 let package = Package(
     name: "Stosyk",
@@ -6,11 +10,18 @@ let package = Package(
         Target(name: "AppLogic"),
         Target(name: "App", dependencies: ["AppLogic"])
     ],
-    dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 5),
-        .Package(url: "https://github.com/vapor/postgresql-provider", majorVersion: 1, minor: 0),
-        .Package(url: "https://github.com/vapor/sqlite-provider", majorVersion: 1, minor: 1)
-    ],
+    dependencies: {
+        var items: [Package.Dependency] = [
+            .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 5),
+            .Package(url: "https://github.com/vapor/postgresql-provider", majorVersion: 1, minor: 0)
+        ]
+        if addTestDependencies {
+            items += [
+                .Package(url: "https://github.com/vapor/sqlite-provider", majorVersion: 1, minor: 1)
+            ]
+        }
+        return items
+    }(),
     exclude: [
         "Config",
         "Localization",
